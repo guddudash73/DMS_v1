d9426b185184386ff547629c55b474d06ddc0e5b
guddudash73 <guddudash73@gmail.com>
2025-11-21 13:21:20 +0530

    feat(ci): this is for ci test10

--- packages/config/env.d.ts
+++ packages/config/env.d.ts
@@ -1,5 +1,27 @@
-import type { z } from 'zod';
+import { z } from 'zod';
 
-export const EnvSchema: z.ZodTypeAny;
+export declare const EnvSchema: z.ZodObject<
+  {
+    NODE_ENV: z.ZodDefault<
+      z.ZodEnum<{
+        development: 'development';
+        test: 'test';
+        production: 'production';
+      }>
+    >;
+    PORT: z.ZodDefault<z.ZodString>;
+    CORS_ORIGIN: z.ZodOptional<z.ZodString>;
+    AWS_REGION: z.ZodDefault<z.ZodString>;
+    DYNAMO_ENDPOINT: z.ZodDefault<z.ZodString>;
+    S3_ENDPOINT: z.ZodDefault<z.ZodString>;
+    XRAY_BUCKET_NAME: z.ZodString;
+    ACCESS_TOKEN_TTL_SEC: z.ZodDefault<z.ZodCoercedNumber<unknown>>;
+    REFRESH_TOKEN_TTL_SEC: z.ZodDefault<z.ZodCoercedNumber<unknown>>;
+    DDB_TABLE_NAME: z.ZodString;
+  },
+  z.core.$strip
+>;
+
 export type Env = z.infer<typeof EnvSchema>;
-export function parseEnv(raw: NodeJS.ProcessEnv): Env;
+
+export declare const parseEnv: (raw: NodeJS.ProcessEnv) => Env;
