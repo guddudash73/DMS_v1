18ce8eac0aee62985d5c32e7908fcb397143413f
guddudash73 <guddudash73@gmail.com>
2025-11-21 13:18:04 +0530

    feat(ci): this is for ci test9

--- packages/config/env.d.ts
+++ packages/config/env.d.ts
@@ -1,1 +1,5 @@
-export * from './dist/env';
+import type { z } from 'zod';
+
+export const EnvSchema: z.ZodTypeAny;
+export type Env = z.infer<typeof EnvSchema>;
+export function parseEnv(raw: NodeJS.ProcessEnv): Env;
